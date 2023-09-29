fefa74eb36cfa5ba0f8bbc33e6d470cc3a84fbec
Karishma Pawar <kspatil1693@gmail.com>
2023-09-30 01:11:00 +0530

    updatetwo

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,15 +1,15 @@
-import React from 'react'
-
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const Navbar = () => {
   return (
     <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary ">
+        <nav className="navbar  collapseOnSelect navbar-expand-lg navbar-dark bg-primary ">
   <div className="container-fluid">
     <a className="navbar-brand m-2  navdata" href="/">Crawford2U</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse"id="basic-navbar-nav" >
+    <div className=" navbar-collapse"id="basic-navbar-nav" >
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
            <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle text-white" href="/" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
