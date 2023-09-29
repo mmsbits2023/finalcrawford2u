52ad8b775f1e1464c50e247ea031a4355fabc00d
Karishma Pawar <kspatil1693@gmail.com>
2023-09-29 23:28:35 +0530

    updatetwo

--- Backend/Controller/AgentClient.js
+++ Backend/Controller/AgentClient.js
@@ -152,10 +152,7 @@ exports.registerClient = (async (request, response, next) => {
         var pindata1 = pindata.slice(1, 4);
         console.log("pindata1", pindata1);
 
-         if (pincode && typeof pincode === 'string' && pincode.length >= 6) {
-             var clientCodeData = pincode.slice(3, 6);
-             return clientCodeData;
-        }
+        var clientCodeData = pincode.slice(3, 6);
         console.log("clientCodeData....", clientCodeData);
         if (pindata1 == clientCodeData) {
             const clientDetailsCheck = new ClientDetails();
