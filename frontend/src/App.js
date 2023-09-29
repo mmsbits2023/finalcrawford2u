71b3c7d619719e9c5df509fb5507ea0a335de9ac
Karishma Pawar <kspatil1693@gmail.com>
2023-09-30 00:58:12 +0530

    updatetwo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar';
 import PageNotFound from './Components/PageNotFound';
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" >
+    <div className="collapse navbar-collapse"id="basic-navbar-nav" >
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
            <li className="nav-item dropdown">
           <a className="nav-link dropdown-toggle text-white" href="/" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
